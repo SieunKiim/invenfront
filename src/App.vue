7c71e5a112e3c3ea72084a2ed9573676ccf1b520
sieun <sieun960513@naver.com>
2022-01-20 11:11:38 +0900

    dd

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Sidebar from '@/components/sidebar/Sidebar'
+import { sidebarWidth } from '@/components/sidebar/state'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components: { Sidebar},
+  setup() {
+    return {sidebarWidth}
   }
 }
 </script>
 
+<template>
+  <Sidebar />
+  <div :style="{ 'margin-left': sidebarWidth}">
+    <router-view />
+  </div>
+</template>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,9 @@
+import VueTouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    routes: [
+
+    ]
+})
