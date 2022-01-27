15bdc9d79bab382332326269477846184353754b
sieun <sieun960513@naver.com>
2022-01-27 17:11:22 +0900

    부트스트랩 적용, 검색어 입력창 완성

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,29 @@
 <template>
-  <!-- <input class="stage-search" type="text" v-model="search" placeholder="사용자 검색" @input="handleSearchInput" @keydown.tab="KeydownTab" /> -->
 <div>
-<h2>여긴 홈이여</h2>
-    <table>
-      <tr>
-        <th>이름</th>
-        <th>부서</th>
-        <th>직책</th>
-        <th>장비</th>
-      </tr>
-      <tr v-for="item in All_list" :key="item.user.name">
-        <td>{{item.user.name}}</td>
-        <td>{{item.user.department}}</td>
-        <td>{{item.user.position}}</td>
-        <td>{{item.tool_name}}</td>
-      <!-- <h2>{{item.user.name}}</h2> -->
-      </tr>
+  <div>
+    <input type="text" v-model="search" placeholder="사용자 검색">
+  </div>
+  <div class="table-responsive">
+    <table class="table table-classic table-hover">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">이름</th>
+          <th scope="col">부서</th>
+          <th scope="col">장비</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="tool in All_data" v-bind:key="tool.id">
+          <th scope="row">{{ tool.id }}</th>
+          <td>{{ tool.user }}</td>
+          <td>{{ tool.부서 }}</td>
+          <td>{{ tool.tool_type }}</td>
+        </tr>
+      </tbody>
     </table>
   </div>
+</div>
 </template>
 
 <script>
@@ -27,31 +33,36 @@ let url = "http://127.0.0.1:8000/inven/All/";
 export default {
   data: () => {
     return {
-      All_list: []
+      search: "",
+      All_list: [],
     };
   },
-  components: {
-
-  },
+  components: {},
   mounted() {
     axios({
       method: "GET",
-      url: url
+      url: url,
     })
-    .then(response => {
-      this.All_list = response.data;
-      console.log(this.All_list);
-    })
-    .catch(response => {
-      console.log("Failed", response)
-
-    });
+      .then((response) => {
+        this.All_list = response.data;
+        console.log(this.All_list);
+      })
+      .catch((response) => {
+        console.log("Failed", response);
+      });
+  },
+  computed: {
+    All_data() {
+      return this.All_list.filter(tool =>
+        tool.user.includes(this.search));
+    }
   }
-}
-
+};
 </script>
 
 <style>
-
-
+.table-responsive {
+  padding: 3em;
+  margin-top: 5em;
+}
 </style>
